d6e5c7174dfe27043db8d026afeff98b299b0174
Akshaan Kakar <akshaan.crackers@gmail.com>
2016-04-07 00:38:47 -0400

    Added implementations for tiger and clime algorithms

--- tiger_clime.R
+++ tiger_clime.R
@@ -5,10 +5,15 @@ args <- commandArgs(TRUE)
 
 #### Read in the data table
 data <- read.csv(args[1],header=TRUE)
+data[,1] <- NULL
+data[,2] <- NULL
+data <- as.matrix(data)
 
 #### Use the TIGER algorithm to estimate the sparse precision matrix
-results <- sugm(data, method ="tiger")
+results <- sugm(data, method =args[2])
 plot(results)
 
 #### Choose graph with maximal sparsity and plot the results
-
+sparsity <- results$sparsity
+ind <- which.max(sparsity)
+sugm.plot(results$path[[ind]])
